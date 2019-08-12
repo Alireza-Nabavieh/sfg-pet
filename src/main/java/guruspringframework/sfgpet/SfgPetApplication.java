65f8ea1c58e54b72b84465d7e11facb1e7bd301d
Alireza <nabavieh@gmail.com>
2019-08-12 22:59:41 +0430

    Initial Project Add

---
+++ src/main/java/guruspringframework/sfgpet/SfgPetApplication.java
@@ -0,0 +1,13 @@
+package guruspringframework.sfgpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetApplication.class, args);
+    }
+
+}
